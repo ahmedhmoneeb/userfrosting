9bc0c012e4faf4575ddda7aa813d898ee5d0fcf6
Hayat <ahmedhamdy395@gmail.com>
2017-05-11 16:51:53 +0200

    last version

---
+++ public/assets/pages/jquery.dashboard_3.js
@@ -0,0 +1,64 @@
+/**
+* Theme: Ubold Admin Template
+* Author: Coderthemes
+* Component: Widget
+* 
+*/
+$( document ).ready(function() {
+    
+    var DrawSparkline = function() {
+        $('#sparkline1').sparkline([0, 23, 43, 35, 44, 45, 56, 37, 40], {
+            type: 'line',
+            width: $('#sparkline1').width(),
+            height: '165',
+            chartRangeMax: 50,
+            lineColor: '#d87878',
+            fillColor: 'transparent',
+            highlightLineColor: 'rgba(0,0,0,.1)',
+            highlightSpotColor: 'rgba(0,0,0,.2)'
+        });
+
+        $('#sparkline1').sparkline([25, 23, 26, 24, 25, 32, 30, 24, 19], {
+            type: 'line',
+            width: $('#sparkline1').width(),
+            height: '165',
+            chartRangeMax: 40,
+            lineColor: '#bcc3c7',
+            fillColor: 'transparent',
+            composite: true,
+            highlightLineColor: 'rgba(0,0,0,1)',
+            highlightSpotColor: 'rgba(0,0,0,1)'
+        });
+    
+        $('#sparkline2').sparkline([3, 6, 7, 8, 6, 4, 7, 10, 12, 7, 4, 9, 12, 13, 11, 12], {
+            type: 'bar',
+            height: '165',
+            barWidth: '10',
+            barSpacing: '3',
+            barColor: 'rgba(16, 120, 99, 0.87)'
+        });
+        
+        $('#sparkline3').sparkline([20, 40, 30, 10], {
+            type: 'pie',
+            width: '165',
+            height: '165',
+            sliceColors: ['#1693a5', '#e9e9e9', '#f3d67d', '#da8080']
+        });
+    
+
+        
+        
+    };
+
+    
+    DrawSparkline();
+    
+    var resizeChart;
+
+    $(window).resize(function(e) {
+        clearTimeout(resizeChart);
+        resizeChart = setTimeout(function() {
+            DrawSparkline();
+        }, 300);
+    });
+});
