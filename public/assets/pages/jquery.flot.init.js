249b0ea4b39008da9ad0903c510d1821424ca692
Hayat <ahmedhamdy395@gmail.com>
2017-05-12 07:32:24 +0200

    charts

--- public/assets/pages/jquery.flot.init.js
+++ public/assets/pages/jquery.flot.init.js
@@ -126,7 +126,7 @@
 			colors : colors,
 			tooltip : true,
 			tooltipOpts : {
-				content : "%s, %p.0%"
+				content : "#777"
 			}
 		};
 
@@ -295,7 +295,7 @@
 				borderWidth : 1,
 				borderColor : "#eeeeee"
 			},
-			colors : ["#6e8cd7", "#34d3eb", "#5fbeaa"],
+			colors : ["#c5e3fa", "#d97c7c", "#5d92b4"],
 			tooltip : true,
 			tooltipOpts : {
 				defaultTheme : false
@@ -336,7 +336,7 @@
 		var uploads = [[0, 9], [1, 8], [2, 5], [3, 8], [4, 5], [5, 14], [6, 10]];
 		var downloads = [[0, 5], [1, 12], [2, 4], [3, 3], [4, 12], [5, 11], [6, 14]];
 		var plabels = ["Visits", "Pages/Visit"];
-		var pcolors = ['#5fbeaa', '#34d3eb'];
+		var pcolors = ['#5d92b4', '#dee8ed'];
 		var borderColor = '#f5f5f5';
 		var bgColor = '#fff';
 		this.createPlotGraph("#website-stats", uploads, downloads, plabels, pcolors, borderColor, bgColor);
@@ -344,7 +344,7 @@
 		//Pie graph data
 		var pielabels = ["Series 1", "Series 2", "Series 3"];
 		var datas = [20, 30, 15];
-		var colors = ["#5fbeaa", "#6c85bd", "#34d3eb"];
+		var colors = ["#f1d06d", "#d97c7c", "#c5e3fa"];
 		this.createPieGraph("#pie-chart #pie-chart-container", pielabels, datas, colors);
 
 		//real time data representation
@@ -363,7 +363,7 @@
 		//Donut pie graph data
 		var donutlabels = ["Series 1", "Series 2", "Series 3", "Series 4"];
 		var donutdatas = [35, 20, 10, 20];
-		var donutcolors = ["#5fbeaa", "#ebeff2", "#34d3eb", "#6e8cd7"];
+		var donutcolors = ["#2d3b63", "#ebeff2", "#a7b3bc", "#2577b5"];
 		this.createDonutGraph("#donut-chart #donut-chart-container", donutlabels, donutdatas, donutcolors);
 
 		//Combine graph data
@@ -483,7 +483,7 @@ $(document).ready(function() {
 					color : '#bdbdbd'
 				}
 			},
-			colors : ["#6e8cd7", "#34d3eb", "#5fbeaa"],
+			colors : ["#d97c7c", "#c5e3fa", "#f1d06d"],
 			tooltip : true, //activate tooltip
 			tooltipOpts : {
 				content : "%s : %y.0",
